d9ccc7e6e3ff1262228fbb95ae1724db3f0a35dc
thai <hongthai2007.hongthai2007@gmail.com>
2022-06-09 16:46:44 +0700

    products without responsive and js

--- dest/main.js
+++ dest/main.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+//Button Menu
+function btnMain(){
+    const btnMenu = document.querySelector(".header__body .header__body-menu");
+    const bgMenu = document.querySelector(".header__nav .nav");
+    btnMenu.addEventListener("click",function(){
+        btnMenu.classList.toggle("active");
+        bgMenu.classList.toggle("active");
+        console.log(bgMenu)
+    })
+}
+btnMain();
+
+//
